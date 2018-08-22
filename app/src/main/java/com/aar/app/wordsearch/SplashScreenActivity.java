2d3d095c1e1a3aa26528cc8fdfe51104de570dee
Abdul Aris <abdularisrahmanudin10@gmail.com>
2018-08-22 18:50:57 +0700

    add splash screen

---
+++ app/src/main/java/com/aar/app/wordsearch/SplashScreenActivity.java
@@ -0,0 +1,19 @@
+package com.aar.app.wordsearch;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.aar.app.wordsearch.mainmenu.MainMenuActivity;
+
+public class SplashScreenActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(this, MainMenuActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
